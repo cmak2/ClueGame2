e8a8adbb06341b6d04bba05e90359817b10488a1
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-02-27 12:21:12 -0700

    Added Experimental test methods

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,5 +1,18 @@
 package tests;
 
+/**
+ * 
+ * @author Calvin Mak
+ * Description: Testing ClueGame
+ * 
+ */
+
+
+
+
+
+
+
 import java.util.*;
 
 import experiment.BoardCell;
@@ -12,4 +25,47 @@ public class IntBoardTests {
 	public void makeBoard() {
 		board = new IntBoard();
 	}
+	
+	public void testSize() {
+		BoardCell Cell = board.getCell(0,0);
+		Set<BoardCell> testList = board.getAdjList(Cell);
+		
+	}
+	
+	
+	/**
+	 * Testing Targets
+	 */
+	
+	//TL Means top left
+	public void testTargetsTL() {
+		
+	}
+	
+	//BRC means Bottom Right Corner
+	public void testTargetsBRC() {
+		
+	}
+	
+	//RE Means Right Edge
+	public void testTargetsRE() {
+		
+	}
+	
+	//LE Means Left Edge
+	public void testTargetsLE() {
+		
+	}
+	
+	//SCM means Second Column Middle of Grid
+	public void testTargetsSCM() {
+		
+	}
+	
+	//SFLC means Second From Last Column
+	public void testTargetsSFLC() {
+		
+	}
+	
+	private IntBoard board;
 }
