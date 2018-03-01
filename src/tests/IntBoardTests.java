c511879ff040290205f6ee8abbfa24ffee078ceb
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-03-01 11:04:36 -0700

    Updated with JUnit

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,5 +1,13 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+import experiment.BoardCell;
+import experiment.IntBoard;
 /**
  * 
  * @author Calvin Mak
@@ -9,23 +17,13 @@ package tests;
 
 
 
-
-
-
-
-import java.util.*;
-
-import experiment.BoardCell;
-import experiment.IntBoard;
-
-import org.junit.*;
-
 public class IntBoardTests {
 	@Before
 	public void makeBoard() {
 		board = new IntBoard();
 	}
 	
+	@Test
 	public void testSize() {
 		BoardCell Cell = board.getCell(0,0);
 		Set<BoardCell> testList = board.getAdjList(Cell);
@@ -38,31 +36,37 @@ public class IntBoardTests {
 	 */
 	
 	//TL Means top left
+	@Test
 	public void testTargetsTL() {
 		
 	}
 	
 	//BRC means Bottom Right Corner
+	@Test
 	public void testTargetsBRC() {
 		
 	}
 	
 	//RE Means Right Edge
+	@Test
 	public void testTargetsRE() {
 		
 	}
 	
 	//LE Means Left Edge
+	@Test
 	public void testTargetsLE() {
 		
 	}
 	
 	//SCM means Second Column Middle of Grid
+	@Test
 	public void testTargetsSCM() {
 		
 	}
 	
 	//SFLC means Second From Last Column
+	@Test
 	public void testTargetsSFLC() {
 		
 	}
