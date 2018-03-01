0316a83d92685a42f612744f78e5181365637061
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-03-01 11:29:57 -0700

    Completed Getters and cleaned up code
    Fixed some functionality and added Class description and game purpose for Coding notes.

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,12 +1,37 @@
 package experiment;
 import java.util.*;
 
+/**
+ * 
+ * 
+ * @author Calvin Mak
+ *
+ */
+
+
+/*
+ * Author Notes:
+ * About Clue:
+ * Clue will be a large gameboard grid
+ * By rolling a die, you calculate how many spaces you can move.
+ * We will have one game board.
+ * There is an array for movement, one space = one value
+ * We cannot move backwards, so keep track of spaces
+ * Targets represent all of the final spaces we can reach rolling the die.
+ * Visited will be spaces visited to prevent going backwards
+ */
+
+
+/*
+ * Info:
+ * Adjacency list calculates each adjacent cell coordinates in the grid
+ */
 
 public class IntBoard {
 	
 	//Default Constructor
 	IntBoard() {
-		
+		calcAdjacencies();
 	}
 	
 	//Calculates the Adjacency list for each grid Cell and stores results in a map
@@ -15,14 +40,13 @@ public class IntBoard {
 	}
 	
 	//Getter
-	getCell(int x, int y) {
-		
+	BoardCell getCell(int x, int y) {
+		return cell;
 	}
 	
 	//Getter
-	Set<BoardCell> getAdjList() {
-	
-		return;
+	Set<BoardCell> getAdjList(BoardCell cell2) {
+		return adjMatrix.get(cell2);
 	}
 	
 	//Calculates targets that are pathLength distance from the startCell. The list of targets are stored as a Set in a variable.
@@ -32,13 +56,13 @@ public class IntBoard {
 	
 	//Returns a list of targets as a set
 	Set<BoardCell> getTargets() {
-		
-		return;
+		return targets;
 	}
 	
 	//Adjacent Matrix
 	private Map<BoardCell, Set<BoardCell>> adjMatrix;
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
+	private BoardCell cell;			//Keeps track of current cell
 	
 }
