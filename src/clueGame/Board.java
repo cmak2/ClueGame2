fc90e958d29a290f5a79f8a52f35af4ee5753e87
Calvin <cmak@mymail.mines.edu>
2018-03-09 22:57:03 -0700

    New Files/redid some datastructures.

---
+++ src/clueGame/Board.java
@@ -0,0 +1,70 @@
+package clueGame;
+
+import java.awt.*;
+import java.util.*;
+
+/**
+ * 
+ * @author 
+ *
+ */
+
+/*
+ * 
+ * Notes:
+ * Board class.
+ * Contains only one instance of the gameboard. Therefore, the singleton method is used.
+ * Max size is an estimate to allocate however much memory will be needed for the gameboard.
+ * 
+ * 
+ */
+
+public class Board {
+	
+	//public methods
+	public static Board getInstance() {
+		if (boardz == null) {
+			boardz = new Board();
+		}
+		return boardz;
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadRoomConfig() {
+		
+	}
+	
+	public void loadBoardConfig() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void calcTargets(BoardCell cell, int pathLength) {
+		
+	}
+	
+	//Creates constructor
+	private Board() {}
+
+
+	//Public Variables (Static and final)
+	public static final int MAX_BOARD_SIZE = 50;		//50 x 50 gameboard
+	
+	//Private Variables and Datastructures
+	private static final int NUM_ROWS;
+	private static final int NUM_COLUMNS;
+
+	private static Board boardz;
+	private BoardCell board;
+	private Map<Character, String> legend;
+	private Map<BoardCell, Set<BoardCell>> adjMatrix;
+	private Set<BoardCell> targets;
+	private String boardConfigFile;
+	private String roomConfigFile;
+}
---
+++ src/tests/ClueGameTest_File.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import experiment.*;
+import clueGame.*;
+
+class ClueGameTest_File {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
