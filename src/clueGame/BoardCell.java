0434e5d0055eec3c133c6733420a8d7a872f6b87
Calvin <cmak@mymail.mines.edu>
2018-03-11 21:40:49 -0600

    Completed getDoorDirection methods

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -37,14 +37,38 @@ public class BoardCell {
 	}
 	
 	public DoorDirection getDoorDir() {	//What Direction
-		return null;
+		if (initial.length() > 1)
+		{
+			if (initial.charAt(1) == 'U') {
+				return DoorDirection.UP;
+			} else if (initial.charAt(1) == 'D') {
+				return DoorDirection.DOWN;
+			} else if (initial.charAt(1) == 'R') {
+				return DoorDirection.RIGHT;
+			} else if (initial.charAt(1) == 'L') {
+				return DoorDirection.LEFT;
+			}
+		}
+		return DoorDirection.NONE;
 	}
 	
-	public DoorDirection getDoorDirection() {
-		
+	public DoorDirection getDoorDirection() {				//Same as method above to accommodate both test files.
+		if (initial.length() > 1)
+		{
+			if (initial.charAt(1) == 'U') {
+				return DoorDirection.UP;
+			} else if (initial.charAt(1) == 'D') {
+				return DoorDirection.DOWN;
+			} else if (initial.charAt(1) == 'R') {
+				return DoorDirection.RIGHT;
+			} else if (initial.charAt(1) == 'L') {
+				return DoorDirection.LEFT;
+			}
+		}
+		return DoorDirection.NONE;
 	}
 	
-	public String getInitial() {
+	public String getInitial() {		//There could be more than one character for the initial, for example doors and other external elements.
 		return initial;
 	}
 	
